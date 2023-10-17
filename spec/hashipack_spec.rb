30807312811941cec7f01cdb161f8c82541a376a
Lukas_Skywalker <LukasSkywalker@users.noreply.github.com>
2023-10-17 20:12:16 +0200

    Fix specs after rename

--- spec/hashipack_spec.rb
+++ spec/hashipack_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-RSpec.describe hashipack do
+RSpec.describe Hashipack do
   it "has a version number" do
-    expect(hashipack::VERSION).not_to be nil
+    expect(Hashipack::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  xit "does something useful" do
     expect(false).to eq(true)
   end
 end
