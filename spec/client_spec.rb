aea182c924254a53fdfa28f24795bd228a85cbaa
Lukas_Skywalker <git@lukasdiener.ch>
2024-08-28 11:01:12 +0200

    Install packer plugin for GHA

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Hashipack::Client do
   it 'runs packer' do
     packer_client = Hashipack::Client.new
 
-    print_message = -> (message) { puts message }
-    print_progress = -> (progress) { puts progress * 100 }
+    print_message = ->(message) { puts message }
+    print_progress = ->(progress) { puts progress * 100 }
 
     packer_client.build('spec/docker.pkr.hcl', on_output: print_message, on_progress: print_progress, estimated_duration: 5)
   end
